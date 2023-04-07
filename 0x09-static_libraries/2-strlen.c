b594c335623494631ba88fc92cb3953633eb15a3
ahmedashry3 <ahmed_engineer2009@hotmail.com>
2023-04-07 15:32:11 -0700

    “update”

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,2 +1,18 @@
 #include "main.h"
-int _strlen(char *s);
+/**
+* _strlen - Length of string
+* @s: string
+* Return: length
+*/
+int _strlen(char *s)
+{
+int longi = 0;
+
+while (*s != '\0')
+{
+longi++;
+s++;
+}
+
+return (longi);
+}
