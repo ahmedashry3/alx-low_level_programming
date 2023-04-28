d9de807eb7f42d15fecb42046798b3d9123deb08
ahmedashry3 <ahmed_engineer2009@hotmail.com>
2023-04-28 12:11:45 -0700

    “lists.h”

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
@@ -19,3 +22,4 @@ size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
 #endif
+
