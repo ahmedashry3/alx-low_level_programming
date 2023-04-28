c123afdb0cc3386c8ae9dcd32dc25729fe0e51e8
ahmedashry3 <ahmed_engineer2009@hotmail.com>
2023-04-28 12:43:04 -0700

    “lists.h”

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,6 +4,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * struct listint_s - singly linked list.
+ * @n: Integer.
+ * @next: points to next node.
+ *
+ * Description: singly linked list node structure for ALX.
+ */
+typedef struct listint_s
+{
+int n;
+struct listint_s *next;
+} listint_t;
+
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -22,4 +35,3 @@ size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
 #endif
-
